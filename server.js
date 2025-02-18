d820d9e73cc02606eecbaf7fbf624fde4b43f359
Bss-bass <sivapat13@gmail.com>
2025-02-19 00:29:52 +0700

    First Commit Create Project

---
+++ server.js
@@ -0,0 +1,15 @@
+// const { Pool } = require('pg');
+
+// const db = new Pool({
+//     host: 'localhost',
+//     port: 5432,
+//     user: 'siwapat',
+//     password: 'Bass10101',
+//     database: 'siwapat'
+// });
+
+// db.on('connect', () => {
+//     console.log('✅ Connected to PostgreSQL database');
+// });
+
+// module.exports = db;
